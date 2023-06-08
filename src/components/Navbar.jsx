1d8a7e8b39bf53ee5710203c09842cdbd2d4cd68
Kay-El-41 <z.khantolino@gmail.com>
2023-06-08 12:15:22 +0630

    almost finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import { useEffect, useState } from 'react'
 import './styles/navbar.css'
+
+// icons
 import { IoCartOutline } from 'react-icons/io5'
 import { HiMenuAlt2 } from 'react-icons/hi'
 import { FiSearch } from 'react-icons/fi'
-import { useEffect, useState } from 'react'
 
 const Navbar = () => {
   const [scrolled, setScroll] = useState()
+
+  // turn dark background when scrolled
   useEffect(() => {
     document.addEventListener('scroll', () => {
       if (window.scrollY > 400) {
@@ -23,12 +27,15 @@ const Navbar = () => {
         backgroundColor: scrolled ? '#333' : 'transparent',
       }}
     >
-      <img src="/logo.png" alt="" height={20} />
+      <div className="nav-container">
+        <img src="/logo.png" alt="" height={20} />
 
-      <div className="nav-links">
-        <FiSearch className="nav-link-icon" />
-        <IoCartOutline className="nav-link-icon" />
-        <HiMenuAlt2 className="nav-link-icon" />
+        <div className="nav-links">
+          <FiSearch className="nav-link-icon" />
+          <IoCartOutline className="nav-link-icon" />
+          <HiMenuAlt2 className="nav-link-icon" />
+        </div>
+
       </div>
     </nav>
   )
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,85 @@
+import { GrPowerReset, GrLocation } from 'react-icons/gr'
+import { MdOutlinePolicy } from 'react-icons/md'
+import { TbPhone } from 'react-icons/tb'
+import './styles/footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer-section">
+      <div className="footer-container">
+        <div className="footer-customers">
+          <h5 className="footer-title">Customer Service</h5>
+          <ul>
+            <li>
+              <MdOutlinePolicy />
+              &nbsp;Terms & Privacy Policy
+            </li>
+            <li>
+              <GrPowerReset />
+              &nbsp;Return Policy
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-payment">
+          <h5 className="footer-title">Payment</h5>
+          <img src="/footer/kbzpay.png" alt="" />
+          <img src="/footer/wavepay.png" alt="" />
+        </div>
+
+        <div className="footer-language">
+          <h5 className="footer-title">Language</h5>
+          <form action="">
+            <label htmlFor="mmuni">
+              <input type="radio" name="lang" id="mmuni" />
+              Myanmar (Unicode)
+            </label>
+            <label htmlFor="mmzaw">
+              <input type="radio" name="lang" id="mmzaw" />
+              Myanmar (Zawgyi)
+            </label>
+            <label htmlFor="en">
+              <input type="radio" name="lang" id="en" checked />
+              English
+            </label>
+          </form>
+        </div>
+
+        <div className="footer-follow">
+          <h5 className="footer-title">Follow Us On</h5>
+          <img src="/footer/social1.png" alt="" />
+          <img src="/footer/social2.png" alt="" />
+          <img src="/footer/social3.png" alt="" />
+          <img src="/footer/social4.png" alt="" />
+          <img src="/footer/social5.png" alt="" />
+        </div>
+
+        <div className="footer-contact">
+          <h5 className="footer-title">Contact Us</h5>
+          <ul>
+            <li className="address">
+              <div className="address-icon">
+                <GrLocation />
+              </div>
+              &nbsp;Lay Daunt Kan Main Road, Cashmere Stop, Near Zawana,
+              Thingangyun Tsp, Yangon
+            </li>
+            <li>
+              <TbPhone />
+              &nbsp;09458489458
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-download">
+          <h5 className="footer-title">Download Our App</h5>
+          <img src="/footer/qrcode.png" alt="" />
+        </div>
+
+        <div className="footer-copyright">
+          @ Copyright 2023 C by D Co.,Ltd. All rights reserved.
+        </div>
+      </div>
     </div>
   )
 }
